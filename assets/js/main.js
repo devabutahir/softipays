cdd822b532da887b69ce8bfbfbb51e308d1c5141
devabutahir <devtahir242@gmail.com>
2024-09-05 18:23:17 +0600

    globe-adding

--- assets/js/main.js
+++ assets/js/main.js
@@ -388,40 +388,188 @@ document.addEventListener("DOMContentLoaded", function () {
   //   });
   // });
 
-
+//Select Dropdown
   $(function () {
 
-    // Initialize dropdowns
-    $('.containerss').each(function () {
-        var container = $(this); // Scope to the current container
-        var ulList = container.find('.ulList');
-        var ulImg  = container.find('.ulImg');
-        var ulTxt  = container.find('.ulTxt');
-        var liList = container.find('.liList');
-        var liDiv  = container.find('.li');
-        var liImg  = container.find('.liImg');
+      // Initialize dropdowns
+      $('.containerss').each(function () {
+          var container = $(this); // Scope to the current container
+          var ulList = container.find('.ulList');
+          var ulImg  = container.find('.ulImg');
+          var ulTxt  = container.find('.ulTxt');
+          var liList = container.find('.liList');
+          var liDiv  = container.find('.li');
+          var liImg  = container.find('.liImg');
 
-        // Hide Li List When Click Away //
-        $(document).on('mouseup', function (event) {
-            if (!ulList.is(event.target) && ulList.has(event.target).length === 0 && !liList.is(event.target) && liList.has(event.target).length === 0) {
-                liList.slideUp(); // Hide with smooth slide animation
-            }
-        });
+          // Hide Li List When Click Away //
+          $(document).on('mouseup', function (event) {
+              if (!ulList.is(event.target) && ulList.has(event.target).length === 0 && !liList.is(event.target) && liList.has(event.target).length === 0) {
+                  liList.slideUp(); // Hide with smooth slide animation
+              }
+          });
 
-        // Show And Hide Li List with Smooth Animation //
-        ulList.on('click', function () {
-            liList.slideToggle(); // Toggle with smooth slide animation
-        });
+          // Show And Hide Li List with Smooth Animation //
+          ulList.on('click', function () {
+              liList.slideToggle(); // Toggle with smooth slide animation
+          });
 
-        // Put Li List Img And Text In Ul List //
-        liDiv.on('click', function () {
-            ulImg.attr('src', $(this).children('img').attr('src')).show();
-            ulTxt.text($(this).text());
-            liList.slideUp(); // Hide the list with animation after selection
-        });
-    });
+          // Put Li List Img And Text In Ul List //
+          liDiv.on('click', function () {
+              ulImg.attr('src', $(this).children('img').attr('src')).show();
+              ulTxt.text($(this).text());
+              liList.slideUp(); // Hide the list with animation after selection
+          });
+      });
 
-});
+  });
+  //Global Chart
+  
+  // Themes begin
+  // am4core.useTheme(am4themes_animated);
+  // Themes end
+
+  var mapChart = am4core.create("chartdiv", am4maps.MapChart);
+
+  // Set map definition
+  mapChart.geodata = am4geodata_continentsLow;
+
+  var colorSet = new am4core.ColorSet();
+  var currentTime = new Date();
+
+  // Set projection
+  mapChart.projection = new am4maps.projections.Orthographic();
+  mapChart.panBehavior = "rotateLongLat";
+  mapChart.deltaLatitude = -30;
+  mapChart.deltaLongitude = 60;
+  // chart.padding(20,20,20,20);
+
+  // Create map polygon series
+  var polygonSeries = mapChart.series.push(new am4maps.MapPolygonSeries());
+
+  // Make map load polygon (like country names) data from GeoJSON
+  polygonSeries.useGeodata = true;
+
+  // Configure series
+  var polygonTemplate = polygonSeries.mapPolygons.template;
+  polygonTemplate.tooltipText = "{name}";
+  polygonTemplate.fill = am4core.color("#47c78a");
+  polygonTemplate.stroke = am4core.color("#454a58");
+  polygonTemplate.strokeWidth = 0.5;
+
+  var graticuleSeries = mapChart.series.push(new am4maps.GraticuleSeries());
+  graticuleSeries.mapLines.template.line.stroke = am4core.color("#ffffff");
+  graticuleSeries.mapLines.template.line.strokeOpacity = 0.08;
+  graticuleSeries.fitExtent = false;
+
+
+  mapChart.backgroundSeries.mapPolygons.template.polygon.fillOpacity = 0.1;
+  mapChart.backgroundSeries.mapPolygons.template.polygon.fill = am4core.color("#ffffff");
+
+  // Shared
+  var hoverColorHex = "#9a7bca";
+  var hoverColor = am4core.color(hoverColorHex);
+
+  // Continents 
+  var continentsSeries = mapChart.series.push(new am4maps.MapPolygonSeries());
+  continentsSeries.geodata = am4geodata_continentsLow;
+  continentsSeries.useGeodata = true;
+  // continentsSeries.exclude = ["antarctica"];
+
+  var continentTemplate = continentsSeries.mapPolygons.template;
+  continentTemplate.tooltipText = "{name}: {population.formatNumber('#,000')}";
+  continentTemplate.properties.fillOpacity = 0.8; // Reduce conflict with back to continents map label
+  continentTemplate.propertyFields.fill = "color";
+  continentTemplate.nonScalingStroke = true;
+  continentTemplate.strokeOpacity = 0.5;
+  continentTemplate.events.on("hit", function(event){
+    if ( ! countriesSeries.visible) 
+      countriesSeries.visible = true;
+    mapChart.zoomToMapObject(event.target);
+    // stateTemplate.show();
+    countryTemplate.show();
+      // imageSeriesTemplate.show();
+        imageSeriesTemplate2.show();
+    //    imageSeriesTemplate3.show();
+    // imageSeriesTemplate4.show();
+    labelContainer.show();
+    
+  });
+
+  var contintentHover = continentTemplate.states.create("hover");
+  contintentHover.properties.fill = hoverColor;
+  contintentHover.properties.stroke = hoverColor;
+
+  continentsSeries.dataFields.zoomLevel = "zoomLevel";
+  continentsSeries.dataFields.zoomGeoPoint = "zoomGeoPoint";
+
+
+  continentsSeries.data = [ {
+    "id": "africa",
+    "population": "1287920000",
+    "color": mapChart.colors.getIndex(0),
+    "zoomLevel": 2,
+    "zoomGeoPoint": {
+      "latitude": 18,
+      "longitude": 20
+    }
+  }, {
+    "id": "asia",
+    "population": "4545133000",
+    "color": mapChart.colors.getIndex(1),
+    "zoomLevel": 2,
+    "zoomGeoPoint": {
+      "latitude": 46,
+      "longitude": 89
+    }
+  }, {
+    "id": "oceania",
+    "population": "41261000",
+    "color": mapChart.colors.getIndex(2),
+    "zoomLevel": 2,
+    "zoomGeoPoint": {
+      "latitude": -7,
+      "longitude": 129
+    }
+  }, {
+    "id": "europe",
+    "population": "742648000",
+    "color": mapChart.colors.getIndex(3),
+    "zoomLevel": 2,
+    "zoomGeoPoint": {
+      "latitude": 48,
+      "longitude": 10
+    }
+  }, {
+    "id": "northAmerica",
+    "population": "587615000",
+    "color": mapChart.colors.getIndex(4),
+    "zoomLevel": 5,
+    "zoomGeoPoint": {
+      "latitude": 38,
+      "longitude": -90
+    }
+  }, {
+    "id": "southAmerica",
+    "population": "428240000",
+    "color": mapChart.colors.getIndex(5),
+    "zoomLevel": 2,
+    "zoomGeoPoint": {
+      "latitude": -9,
+      "longitude": -60
+    }
+  }];
+
+
+let animation;
+setTimeout(function(){
+  animation = mapChart.animate({property:"deltaLongitude", to:100000}, 20000000);
+}, 6000)
+
+mapChart.seriesContainer.events.on("down", function(){
+if(animation){
+  animation.stop();
+}
+})
 
 
   });
